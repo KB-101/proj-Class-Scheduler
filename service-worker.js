bc5eee2fcb361b7a1fcff132c4c22f78580ae2c7
Kaleab Solomon <kaleabsolomon59@gmail.com>
2025-11-17 19:17:19 +0300

    Implement service worker for caching files

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "class-scheduler-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/service-worker.js",
+  "/style.css",
+  "/script.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+});
